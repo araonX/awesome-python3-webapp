aedb5a75b90b23dea77e09a4df89283260e58ef3
xiejg <xiejg@VM_0_13_centos.(none)>
2018-03-21 18:01:37 +0800

    add twice

---
+++ www/test.py
@@ -0,0 +1,14 @@
+#!/home/env3.5/bin/python3.5
+import orm
+from models import User, Blog, Comment
+import asyncio
+async def test(loop, **kw):
+    await orm.create_pool(loop=loop,user='www-data', password='www-data', db='awesome')
+    u = User(name=kw.get('name'), email=kw.get('email'), passwd=kw.get('passwd'), image=kw.get('image'), id=kw.get('id'))
+    await u.save()
+    await orm.destory_pool()
+
+data=dict(name='xjg', email='1011000@qq.com', passwd='13241', image='about:blank', id='1245')
+loop=asyncio.get_event_loop()
+loop.run_until_complete(test(loop,**data))
+loop.close()
