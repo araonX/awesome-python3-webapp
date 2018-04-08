d2a8704950a78121191ec747c43ab9fd503f897f
root <root@VM_0_13_centos.(none)>
2018-04-08 13:37:38 +0800

    end

--- www/coroweb.py
+++ www/coroweb.py
@@ -1,5 +1,6 @@
-#!/home/xiejg/awesome-python3-webapp/env/bin/python3.5
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 __author__ = 'Michael Liao'
 
 import asyncio, os, inspect, logging, functools
@@ -87,7 +88,8 @@ class RequestHandler(object):
         self._named_kw_args = get_named_kw_args(fn)
         self._required_kw_args = get_required_kw_args(fn)
 
-    async def __call__(self, request):
+    @asyncio.coroutine
+    def __call__(self, request):
         kw = None
         if self._has_var_kw_arg or self._has_named_kw_args or self._required_kw_args:
             if request.method == 'POST':
@@ -95,12 +97,12 @@ class RequestHandler(object):
                     return web.HTTPBadRequest('Missing Content-Type.')
                 ct = request.content_type.lower()
                 if ct.startswith('application/json'):
-                    params = await request.json()
+                    params = yield from request.json()
                     if not isinstance(params, dict):
                         return web.HTTPBadRequest('JSON body must be object.')
                     kw = params
                 elif ct.startswith('application/x-www-form-urlencoded') or ct.startswith('multipart/form-data'):
-                    params = await request.post()
+                    params = yield from request.post()
                     kw = dict(**params)
                 else:
                     return web.HTTPBadRequest('Unsupported Content-Type: %s' % request.content_type)
@@ -134,7 +136,7 @@ class RequestHandler(object):
                     return web.HTTPBadRequest('Missing argument: %s' % name)
         logging.info('call with args: %s' % str(kw))
         try:
-            r = await self._func(**kw)
+            r = yield from self._func(**kw)
             return r
         except APIError as e:
             return dict(error=e.error, data=e.data, message=e.message)
--- www/test.py
+++ www/test.py
@@ -4,11 +4,13 @@ from models import User, Blog, Comment
 import asyncio
 async def test(loop, **kw):
     await orm.create_pool(loop=loop,user='www-data', password='www-data', db='awesome')
-    u = User(name=kw.get('name'), email=kw.get('email'), passwd=kw.get('passwd'), image=kw.get('image'), id=kw.get('id'))
+    u = User(name=kw.get('name'), email=kw.get('email'), passwd=kw.get('passwd'), image=kw.get('image'), id=kw.get('id'), admin=kw.get('admin'))
+#    u = Blog(id = kw.get('id'), user_id=kw.get('user_id'),user_name=kw.get('user_name'),user_image=kw.get('user_image'),name=kw.get('name'),summary=kw.get('summary'),content=kw.get('content'),created_at=kw.get('created_at')) 
     await u.save()
     await orm.destory_pool()
 
-data=dict(name='dyb', email='1011002@qq.com', passwd='13241', image='about:blank', id='1247')
+#data=dict(id='3',user_id='1245',user_name='admin2',user_image='about:blank',name='first article',summary='this is the first article.',content ='tests',created_at=123456.554)
+data =dict(id)
 loop=asyncio.get_event_loop()
 loop.run_until_complete(test(loop,**data))
 loop.close()
